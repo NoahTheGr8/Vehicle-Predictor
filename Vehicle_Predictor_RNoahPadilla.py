26e9db2e8c9603c359548e6115ebcac3ab5c900e
Noah Padilla <npizza10@gmail.com>
2021-05-03 04:01:37 -0600

    covered the edge case on considering images that are not 3 layers deep as well as removing the normalization data preprocessing

--- Vehicle_Predictor_RNoahPadilla.py
+++ Vehicle_Predictor_RNoahPadilla.py
@@ -26,7 +26,11 @@ TODOS:
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import time 
 
+from skimage import transform as tf
+from matplotlib import transforms
+import cv2
 import matplotlib.image as mpimg
 import scipy.io
 from scipy import signal
@@ -51,6 +55,24 @@ def histogram_of_gradients(img,bins=12):
         hog.append(np.sum(g_mag[g_dir_bin==i]))
     return np.array(hog)/im.shape[0]/im.shape[1]
 
+def display_correspondences(im0,im1,pts0,pts1,clr_str = 'rgbycmwk'):
+    canvas_shape = (max(im0.shape[0],im1.shape[0]),im0.shape[1]+im1.shape[1],3)
+    canvas = np.zeros(canvas_shape,dtype=type(im0[0,0,0]))
+    canvas[:im0.shape[0],:im0.shape[1]] = im0
+    canvas[:im1.shape[0],im0.shape[1]:canvas.shape[1]]= im1
+    fig, ax = plt.subplots(figsize=(8, 4))
+    ax.imshow(canvas)
+    ax.axis('off')
+    pts2 = pts1+np.array([im0.shape[1],0])
+    for i in range(pts0.shape[0]):
+        ax.plot([pts0[i,0],pts2[i,0]],[pts0[i,1],pts2[i,1]],color=clr_str[i%len(clr_str)],linewidth=1.0)
+    fig.suptitle('Point correpondences', fontsize=16)
+
+def select_matches_ransac(pts0, pts1):
+    H, mask = cv2.findHomography(pts0.reshape(-1,1,2), pts1.reshape(-1,1,2), cv2.RANSAC,5.0)
+    choice = np.where(mask.reshape(-1) ==1)[0]
+    return pts0[choice], pts1[choice]
+
 if __name__ == "__main__":
     
     plt.close("all")
@@ -61,40 +83,30 @@ if __name__ == "__main__":
     image_files_test= os.listdir(image_dir_test)
     
     #----------------------------- LOAD AND NORMALIZE IMAGES FROM TRAIN AND TEST SET --------------------------
+    
+    #TODO - MAKE SURE LATER ON YOU REMOVE THE LABELED DATA THAT CORRESPONDED TO THE IMAGES THAT DIDNT HAVE 3 LAYERS
     X_train = []
     X_test = []
+    train_limit = 5398
     print("\n> Loading training data...")
-    for i,image_file in enumerate(image_files_train[:250]):
+    for i,image_file in enumerate(image_files_train[:train_limit]):
         
-        if i > 0 and i % 50 == 0:
+        if i > 0 and i % 1000 == 0:
             print("Pre-Processed", i , "images from train set")
         
         image = mpimg.imread(image_dir_train + image_file)
         
-        #Normalize the image
-        if np.amax(image)>1:
-            image = (image/255).astype(np.float32)
-            #print('Converted image to float')
-        #print('Image shape',image.shape)#used for debugging
-        #show_image(image,'Original image')#used for debugging
-        X_train.append(np.array(image))
+        if len(image.shape) == 3:
+            X_train.append(np.array(image))
     
     print("\n> Loading test data...")
-    for j,image_file in enumerate(image_files_test[:1000:50]):
-        
+    for j,image_file in enumerate(image_files_test[:1000:10]):
         
         image = mpimg.imread(image_dir_test + image_file)
         
-        #Normalize the image
-        if np.amax(image)>1:
-            image = (image/255).astype(np.float32)
-            #print('Converted image to float')
-        
-        #print('Image shape',image.shape)
-        #show_image(image,'Original image')
-        
-        X_test.append(np.array(image))
-    
+        #checks that the image has 3 layers - discard images with only 2
+        if len(image.shape) == 3:
+            X_test.append(np.array(image))
     
     #----------------------------- CONVERT TRAIN AND TEST IMAGES TO HOG --------------------------
     print("\n> Training set | Image representations -> HOG representations...")
@@ -107,11 +119,6 @@ if __name__ == "__main__":
         
         hog = histogram_of_gradients(x,bins=bins)
         HOGS_train.append(hog)#the training set are now represented using HOG
-        #fig, ax = plt.subplots(2,figsize=(8,8))
-        #fig.suptitle('Image ' + str(i))
-        #ax[0].imshow(x)
-        #ax[1].plot(np.arange(bins)*360/bins,hog)
-        #ax[1].set_ylim([0,np.amax(hog)*1.1])
     
     print("\n> Test set | Image representations -> HOG representations...")
     HOGS_test = []
@@ -149,17 +156,69 @@ if __name__ == "__main__":
     #print(km_ssd)
     
     
-    '''
-    #----------------------------------- PRINT VALUES FROM THEIR CLUSTERS -------------------
+    
+    #----------------------------------- PREDICT WHAT CLUSTERS THE TEST SAMPLES ARE IN -------------------
     pred = km.predict(HOGS_test)
     
-    for i,x in enumerate(X_test):
-        fig, ax = plt.subplots(2,figsize=(8,8))
-        fig.suptitle('Image ' + str(i) + ' assigned to cluster: ' + str(pred[i]))
-        ax[0].imshow(x)
-        ax[1].plot(np.arange(bins)*360/bins,HOGS_test[i])
-        ax[1].set_ylim([0,np.amax(HOGS_test[i])*1.1])
+    orb = cv2.ORB_create()
+    '''
+    image_dir_train = '.\\cars_train\\cars_train\\'
+    image_dir_test = '.\\cars_test\\cars_test\\'
+    image_files_train = os.listdir(image_dir_train)
+    image_files_test= os.listdir(image_dir_test)
     '''
     
-    #---------------------------- USE ORB AND RANSAC TO FIND IDENTICAL VEHICLE
-    
+    for i,x_test_sample in enumerate(X_test[:3]):
+        fig, ax = plt.subplots(2,figsize=(8,8))
+        fig.suptitle('Test image ' + str(i) + ' assigned to cluster: ' + str(pred[i]))
+        
+        keypoints1, descriptors1 = orb.detectAndCompute(np.asarray(x_test_sample, np.uint8),mask=None)
+        
+        best_p0 = None #stores the best point matches on image 1
+        best_p1 = None #stores the best point matches on image 2
+        max_feature_matches = 0 #will be used to count the matched features from two images
+        matched_vehicle = None # stores the array containing the matched vehicle
+        
+        for j,x_train_sample_label in enumerate(km_l):
+            if j%200==0:
+                print("Looked through",j, "images - still looking for best match")
+            #if we look at the cluster which x_test_sample defines itself then we can use ORB +RANSAC to look within cluster to find similar vehicle
+            if pred[i] == x_train_sample_label:
+                #print("x_test_sample: ", x_test_sample.shape, "| X_train[i] shape:", X_train[j].shape)
+                #print("pred:" , pred[i], "| x_train_sample_label: ", x_train_sample_label)
+                
+                #---------------------------- USE ORB AND RANSAC TO FIND IDENTICAL VEHICLE
+                keypoints2, descriptors2 = orb.detectAndCompute(np.asarray(X_train[j], np.uint8),mask=None)
+                
+                # Create BFMatcher object
+                matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+                matches = matcher.match(descriptors1,descriptors2)
+                
+                #find the subest of these matches that can be explained using the ransac | for every image find the best matching image
+            
+                # Extract data from orb objects and matcher
+                dist = np.array([m.distance for m in matches])
+                ind1 = np.array([m.queryIdx for m in matches])
+                ind2 = np.array([m.trainIdx for m in matches])
+                
+                #TWEAKED BY NOAH - NEEDED TO CHANGE IF GOING THROUGH A LOOP
+                k1 = np.array([p.pt for p in keypoints1])
+                k2 = np.array([p.pt for p in keypoints2])
+                k1 = k1[ind1]
+                k2 = k2[ind2]
+                # keypoints1[i] and keypoints2[i] are a match
+                
+                #---------------------BEST MATCH POINTS FROM BOTH IMAGES
+                p0, p1 = select_matches_ransac( k1, k2 )
+                if (p0.shape[0] > max_feature_matches):
+                    best_p0 = p0
+                    best_p1 = p1
+                    max_feature_matches = p0.shape[0]
+                    matched_vehicle = X_train[j]
+        
+        display_correspondences(x_test_sample,matched_vehicle, best_p0, best_p1)
+        ax[0].imshow(x_test_sample)
+        ax[1].imshow(matched_vehicle)
+        #ax[1].plot(np.arange(bins)*360/bins,HOGS_test[i])
+        #ax[1].set_ylim([0,np.amax(HOGS_test[i])*1.1])
+        #break
